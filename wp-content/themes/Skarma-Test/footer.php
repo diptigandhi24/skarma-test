ee2628993055bac78c2dd8e9fd115387bd4ee0ac
Dipti Gandhi <gandhidipti.s@gmail.com>
2017-04-14 20:54:37 +0530

    footer of the site and index file of the site

---
+++ wp-content/themes/Skarma-Test/footer.php
@@ -0,0 +1,24 @@
+<div class="container-fluid footer-1 text-center">
+<a href="" ><i class="fa fa-facebook-square" aria-hidden="true" style="border-radius: 0px"></i>
+</a>
+<a href="" ><i class="fa fa-instagram" aria-hidden="true" style="border-radius: 0px"></i>
+</a>
+<a href="" ><i class="fa fa-twitter-square" aria-hidden="true" style="border-radius: 0px"></i>
+</a>
+<a href="" ><i class="fa fa-youtube-play" aria-hidden="true" style="border-radius: 0px"></i>
+</a>
+</div>
+<div class="row bg-theme-light-violet ">
+	<div class="container text-center footer-2 ">
+		<ul class="list-unstyled list-inline margin-none">
+			<li class="list-inline-item width-100 "><a href="">Copyright 2016</a></li>
+			<li class="list-inline-item width-100"><a href="">privacy policy</a></li>
+			<li class="list-inline-item width-100"><a href="">legal terms</a></li>
+			<li class="list-inline-item width-100"><a href="">contact </a></li>
+			<li class="list-inline-item width-100"><a href="">offical site</a></li>
+		</ul>
+	</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
